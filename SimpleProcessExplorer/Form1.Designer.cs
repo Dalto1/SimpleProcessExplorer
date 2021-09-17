475f138d96cec3d354c5aaa5a075d4f769c3e421
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2021-09-17 00:33:49 -0400

    Ajoutez des fichiers projet.

---
+++ SimpleProcessExplorer/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace ProcessExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(390, 269);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 269);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(390, 47);
+            this.panel2.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(130, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 47);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Kill";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(260, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Quitter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Actualiser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 316);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(406, 355);
+            this.Name = "Form1";
+            this.Text = "Simple Process Explorer";
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SimpleProcessExplorer/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ProcessExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach (Process proc in Process.GetProcesses())
+            {
+                string process = UppercaseFirst(proc.ProcessName);
+                if (!listBox1.Items.Contains(process))
+                {
+                    listBox1.Items.Insert(i, process);
+                    i++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Voulez-vous quitter?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String processToKill = listBox1.SelectedItem.ToString();
+
+            if (MessageBox.Show("Voulez-vous fermer le processus " + processToKill + " ?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+
+                bool killedSomething = false;
+                try
+                {
+                    foreach (Process P in Process.GetProcessesByName(processToKill))
+                    {
+                        try
+                        {
+                            P.Kill();
+                            killedSomething = true;
+                        }
+                        catch { }
+                    }
+                }
+                catch { }
+                if (killedSomething) MessageBox.Show(processToKill + " a été tué", "Information");
+                else MessageBox.Show("Erreur dans la tentative de tuer " + processToKill, "Information");
+            }
+        }
+        static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+}
